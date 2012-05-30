3665795f0b9128ea8414ffdcad14fdbf778f45d3
John Smith <git@john-smith.me>
2012-05-30 01:17:21 +0100

    turn off APP_DEBUG

--- app_settings.py
+++ app_settings.py
@@ -5,7 +5,7 @@ Settings for my quick'n'dirty IP reporter
 
 __author__ = "John Smith, 2012 <code@john-smith.me>"
 
-APP_DEBUG = True
+APP_DEBUG = False
 UI_DEBUG = True
 APP_URL = "http://ip-check.appspot.com"
 
